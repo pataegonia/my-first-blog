54be831ca0fb963c5bc2b26ce22c09567ae22349
pataegonia <clue9986@gmail.com>
2026-06-04 21:30:47 +0900

    ch18

--- blog/views.py
+++ blog/views.py
@@ -41,3 +41,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def js_test(request):
+    return render(request, 'blog/js_test.html', {})
